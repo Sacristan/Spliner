d8e783b5e1a4f6ef00deef609df0224a98729fe9
BeetrootLabSac <girts.kesteris@beetrootlab.com>
2016-09-12 20:35:09 +0300

    Added basis

---
+++ Assets/Source/SplineMapCameraController.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SplineMapCameraController : MonoBehaviour
+{
+    private Vector3 startPosition = Vector3.zero;
+    private Vector3 endPosition = Vector3.zero;
+
+    Camera _camera;
+
+    void Awake()
+    {
+        _camera = GetComponent<Camera>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))    // swipe begins
+        {
+            startPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+        if (Input.GetMouseButtonUp(0))    // swipe ends
+        {
+            endPosition = _camera.ScreenToWorldPoint(Input.mousePosition);
+        }
+
+        if (startPosition != endPosition && startPosition != Vector3.zero && endPosition != Vector3.zero)
+        {
+            float deltaX = endPosition.x - startPosition.x;
+            float deltaY = endPosition.y - startPosition.y;
+
+            Vector2 delta = new Vector2(deltaX, deltaY);
+
+            //Debug.Log(delta);
+
+            transform.Translate(delta);
+
+            startPosition = Vector3.zero;
+            endPosition = Vector3.zero;
+        }
+    }
+}
