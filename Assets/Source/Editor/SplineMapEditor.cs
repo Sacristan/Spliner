b55fb3ebc90e759c94d71d455737b20383e813d3
BeetrootLabSac <girts.kesteris@beetrootlab.com>
2016-09-12 17:05:12 +0300

    AnchorManager creates anchors from now on

---
+++ Assets/Source/Editor/SplineMapEditor.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(SplineMap))]
+public class SplineMapEditor : Editor
+{
+    SplineMap t;
+    SerializedObject GetTarget;
+
+    void OnEnable()
+    {
+        t = (SplineMap)target;
+        GetTarget = new SerializedObject(t);
+    }
+
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+        GetTarget.Update();
+
+        EditorGUILayout.Space();
+        EditorGUILayout.Space();
+
+        if (GUILayout.Button("Add Anchor"))
+        {
+            AddAnchor();
+        }
+    }
+
+    private void AddAnchor()
+    {
+        GameObject anchorGo = new GameObject("Anchor_"+System.Guid.NewGuid(), typeof(Anchor));
+        Anchor anchor = anchorGo.GetComponent<Anchor>();
+        AnchorSyncer.Sync(anchor);
+
+        anchorGo.transform.SetParent(AnchorsContainer);
+        anchorGo.transform.localPosition = Vector3.zero;
+    }
+
+
+    private Transform AnchorsContainer
+    {
+        get
+        {
+            string containerName = "Anchors";
+            Transform anchorsContainer = t.transform.Find(containerName);
+
+            if(anchorsContainer == null)
+            {
+                anchorsContainer = new GameObject(containerName).transform;
+                anchorsContainer.SetParent(t.transform);
+                anchorsContainer.transform.localPosition = Vector3.zero;
+            }
+
+            return anchorsContainer;
+        }
+    }
+}
